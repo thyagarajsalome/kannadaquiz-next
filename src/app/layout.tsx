80b46ecfa43e9b68ca863bb84db419dd19812030
Thyagaraja <your-github-email@example.com>
2026-08-15 17:38:36 +0530

    Performance Fix: Delay loading of AdSense and Analytics to achieve 90+ Mobile PageSpeed Score

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,16 +74,16 @@ export default function RootLayout({
           async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1813097110898475" 
           crossOrigin="anonymous"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
         />
         {children}
         {gaId && (
           <>
             <Script
               src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-              strategy="afterInteractive"
+              strategy="lazyOnload"
             />
-            <Script id="google-analytics" strategy="afterInteractive">
+            <Script id="google-analytics" strategy="lazyOnload">
               {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
